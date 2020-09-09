b6adc7ef9aed1053cc7326941b3009aeecb7f5c1
ersaumya <ersaumyarout@gmail.com>
2020-09-09 23:57:58 +0530

    nav bar added

--- src/app/Shared/modules/nav-bar/components/nav-bar/nav-bar.component.ts
+++ src/app/Shared/modules/nav-bar/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,8 @@
+import { isLoggedInSelector, isAnonymousSelector, currentUserSelector } from './../../../../../Auth/store/selectors';
+import { CurrentUser } from './../../../../types/current-user';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn$:Observable<boolean>
+  isAnonymous$:Observable<boolean>
+  currentUser$:Observable<CurrentUser | null>
+
+  constructor(private store:Store) { }
 
   ngOnInit(): void {
+    this.isLoggedIn$=this.store.pipe(select(isLoggedInSelector))
+    this.isAnonymous$=this.store.pipe(select(isAnonymousSelector))
+    this.currentUser$=this.store.pipe(select(currentUserSelector))
   }
 
 }
