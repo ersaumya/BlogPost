15f13382d6eded3567b737bf716892c5bec1c710
ersaumya <ersaumyarout@gmail.com>
2020-10-16 07:36:54 +0530

    popular tags effects added

---
+++ src/app/Shared/modules/popular-tags/store/effects/getPopularTags.effect.ts
@@ -0,0 +1,31 @@
+import { PopularTag } from './../../../../types/popular-tag';
+import { getPopularTagsAction, getPopularTagsSuccessAction, getPopularTagsFailureAction } from './../actions/getPopularTags.action';
+import { Injectable } from '@angular/core';
+import { createEffect, Actions, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { switchMap, map, catchError,} from 'rxjs/operators';
+import { PopularTagsService } from '../../service/popular-tags.service';
+
+@Injectable()
+export class GetPopularTagsEffect {
+  constructor(
+    private actions$: Actions,
+    private popularTagService: PopularTagsService
+  ) {}
+
+  getPopularTags$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(getPopularTagsAction),
+      switchMap(() => {
+        return this.popularTagService.getPopularTags().pipe(
+          map((popularTag: PopularTag[]) => {
+            return getPopularTagsSuccessAction({popularTag});
+          }),
+          catchError(() => {
+            return of(getPopularTagsFailureAction());
+          })
+        );
+      })
+    )
+  );
+}
