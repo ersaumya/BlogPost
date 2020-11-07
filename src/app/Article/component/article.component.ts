16a6199a13f215d6f1af3a32f57bdf7aaa7def30
ersaumya <ersaumyarout@gmail.com>
2020-11-07 09:36:11 +0530

    delete article button added

--- src/app/Article/component/article.component.ts
+++ src/app/Article/component/article.component.ts
@@ -12,21 +12,22 @@ import { select, Store } from '@ngrx/store';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Article } from 'src/app/Shared/types/article';
+import { deleteArticleAction } from '../store/actions/deleteArticle.action';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  styleUrls: ['./article.component.scss'],
 })
-export class ArticleComponent implements OnInit ,OnDestroy{
-  slug:string;
-  article:Article;
-  articleSubscription:Subscription;
-  isLoading$:Observable<boolean>;
-  error$:Observable<string | null>;
-  isAuthor$:Observable<boolean>;
+export class ArticleComponent implements OnInit, OnDestroy {
+  slug: string;
+  article: Article;
+  articleSubscription: Subscription;
+  isLoading$: Observable<boolean>;
+  error$: Observable<string | null>;
+  isAuthor$: Observable<boolean>;
 
-  constructor(private store:Store,private route:ActivatedRoute) { }
+  constructor(private store: Store, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.initialiseValues();
@@ -34,35 +35,40 @@ export class ArticleComponent implements OnInit ,OnDestroy{
     this.fetchData();
   }
 
-  initialiseValues():void{
-    this.slug=this.route.snapshot.paramMap.get('slug');
-    this.isLoading$=this.store.pipe(select(isLoadingSelector));
+  initialiseValues(): void {
+    this.slug = this.route.snapshot.paramMap.get('slug');
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector));
     this.error$ = this.store.pipe(select(errorSelector));
     this.isAuthor$ = combineLatest(
       this.store.pipe(select(articleSelector)),
       this.store.pipe(select(currentUserSelector))
     ).pipe(
-      map(([article,currentUser]:[Article | null,CurrentUser | null])=>{
-        if(!article || !currentUser){
-          return false
+      map(([article, currentUser]: [Article | null, CurrentUser | null]) => {
+        if (!article || !currentUser) {
+          return false;
         }
-        return currentUser.username === article.author.username
-    }))
+        return currentUser.username === article.author.username;
+      })
+    );
   }
 
-  initialiseListiner():void{
-    this.articleSubscription =this.store
-    .pipe(select(articleSelector))
-    .subscribe((article:Article | null)=>{
-      this.article=article
-    })
+  initialiseListiner(): void {
+    this.articleSubscription = this.store
+      .pipe(select(articleSelector))
+      .subscribe((article: Article | null) => {
+        this.article = article;
+      });
   }
 
-  fetchData():void{
-    this.store.dispatch(getArticleAction({slug:this.slug}))
+  fetchData(): void {
+    this.store.dispatch(getArticleAction({ slug: this.slug }));
   }
 
-  ngOnDestroy():void{
+  ngOnDestroy(): void {
     this.articleSubscription.unsubscribe();
+  }
+
+  deleteArticle(): void {
+    this.store.dispatch(deleteArticleAction({ slug: this.slug }));
   }
 }
