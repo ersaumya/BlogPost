0dd081c57f85a12d384edaf9dfa51b0dd76ae5de
ersaumya <ersaumyarout@gmail.com>
2020-11-10 11:32:20 +0530

    edit action types

---
+++ src/app/EditArticle/store/actionTypes.ts
@@ -0,0 +1,5 @@
+export enum ActionTypes {
+  UPDATE_ARTICLE = '[Update article] Update Article',
+  UPDATE_ARTICLE_SUCCESS = '[Update article] Update Article success',
+  UPDATE_ARTICLE_FAILURE = '[Update article] Update Article failure',
+}
