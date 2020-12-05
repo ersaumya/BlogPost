bf901e3ebd08d162aeef63b32af5bdea8f156d16
ersaumya <ersaumyarout@gmail.com>
2020-12-05 10:34:46 +0530

    add store to component

--- src/app/Shared/modules/add-to-favorite/component/add-to-favorite.component.ts
+++ src/app/Shared/modules/add-to-favorite/component/add-to-favorite.component.ts
@@ -1,3 +1,5 @@
+import { addToFavoritesAction } from './../store/actions/add-to-favorite.action';
+import { Store } from '@ngrx/store';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
@@ -7,24 +9,30 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class AddToFavoriteComponent implements OnInit {
   favoriteCount: number;
-  isFavorited:boolean;
+  isFavorited: boolean;
 
   @Input() isFavorite: boolean;
   @Input() favoritesCount: number;
   @Input() articleSlug: string;
-  constructor() {}
+  constructor(private store: Store) {}
 
   ngOnInit(): void {
-    this.favoriteCount=this.favoritesCount
-    this.isFavorited=this.isFavorite
+    this.favoriteCount = this.favoritesCount;
+    this.isFavorited = this.isFavorite;
   }
 
-  handleLike():void{
-    if(this.isFavorited){
-      this.favoriteCount=this.favoriteCount - 1
-    }else{
-      this.favoriteCount=this.favoriteCount + 1
+  handleLike(): void {
+    this.store.dispatch(
+      addToFavoritesAction({
+        isFavorited: this.isFavorited,
+        slug: this.articleSlug,
+      })
+    );
+    if (this.isFavorited) {
+      this.favoriteCount = this.favoriteCount - 1;
+    } else {
+      this.favoriteCount = this.favoriteCount + 1;
     }
-    this.isFavorited=!this.isFavorited
+    this.isFavorited = !this.isFavorited;
   }
 }
